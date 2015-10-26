d8e27a552d84276d64a04e7a54c5731c062610aa
anryyett <anryyett@gmail.com>
2015-10-26 23:45:25 +0200

    javaScript - upade lesson5  task3

--- GoIT/javaScript/home/lesson5/task3.js
+++ GoIT/javaScript/home/lesson5/task3.js
@@ -2,6 +2,7 @@
  * Created by Elena on 10/17/2015.
  */
 var numbers = [];
+var number;
 do {
     number = prompt('Пожалуйста введите число больше',0);
     if (number === null || number === "" || isNaN(number)){
