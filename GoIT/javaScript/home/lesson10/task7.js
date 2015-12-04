308bf8647ffc40fd7c3f5aa5a21320f355ae2a34
anryyett <anryyett@gmail.com>
2015-12-04 09:54:10 +0200

    hw - lesson10 (update task 7, task 8)

--- GoIT/javaScript/home/lesson10/task7.js
+++ GoIT/javaScript/home/lesson10/task7.js
@@ -3,10 +3,12 @@
  */
 
 function suffle(arr){
+var randomIndex = 0;
+var itemAtIndex = arr.length-1;
     for (var i = arr.length-1; i >=0; i--) {
 
-        var randomIndex = Math.floor(Math.random()*(i+1));
-        var itemAtIndex = arr[randomIndex];
+        randomIndex = Math.floor(Math.random()*(i+1));
+        itemAtIndex = arr[randomIndex];
 
         arr[randomIndex] = arr[i];
         arr[i] = itemAtIndex;
--- GoIT/javaScript/home/lesson10/task8.js
+++ GoIT/javaScript/home/lesson10/task8.js
@@ -2,10 +2,10 @@
  * Created by Elena on 10/28/2015.
  */
 var arr = ['link', 'menu', 'menu__item', 'menu__item', 'header', 'link', 'footer', 'sidebar', 'link'];
-newarr = [];
+var newarr = [];
 for(var i=0; i<arr.length; i++){
     if(newarr.indexOf(arr[i]) === -1){
         newarr.push(arr[i]);
     }
 }
-console.log(newarr.toString());
+console.log(newarr);
